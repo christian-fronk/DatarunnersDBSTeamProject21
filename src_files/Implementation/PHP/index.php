50fd46aa98c722a47b700425f7234ad3af5142df
FishDentistry <cfronk12@gmail.com>
2021-12-01 18:37:07 -0500

    Added css for index page

--- src_files/Implementation/PHP/index.php
+++ src_files/Implementation/PHP/index.php
@@ -1,19 +1,27 @@
-<h1>Welcome to the Pokemon League!</h1>
-<h3>Click a link to view its corresponding table!</h3>
-<hr>
-<ul>
-    <li><a href="/GroupPHP/trainers.php">Trainers</a></li>
-    <li><a href="/GroupPHP/pokemon.php">Pokemon</a></li>
-    <li><a href="/GroupPHP/challenges.php">Challenges</a></li>
-    <li><a href="/GroupPHP/eliteFour.php">Elite Four</a></li>
-    <li><a href="/GroupPHP/hallOfFame.php">Hall of Fame</a></li>
-    <li><a href="/GroupPHP/items.php"> Items</a></li>
-    <li><a href="/GroupPHP/soldItems.php">Sold Items</a></li>
-    <li><a href="/GroupPHP/itemsUsed.php">Items Used</a></li>
-    <li><a href="/GroupPHP/pokemonSpecies.php">Pokemon Species</a></li>
-    <li><a href="/GroupPHP/pokemonSpeciesTypes.php">Pokemon Species Types</a></li>
-    <li><a href="/GroupPHP/pokemonTypes.php">Pokemon Types</a></li>
-    <li><a href="/GroupPHP/pokemonParticipants.php">Pokemon Participants</a></li>
-</ul>
+   
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pokemon League Database</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <h1>Welcome to the Pokemon League!</h1>
+    <h3>Click a link to view its corresponding table!</h3>
+    <hr>
+    <ul>
+        <li><a href="/GroupPHP/trainers.php">Trainers</a></li>
+        <li><a href="/GroupPHP/pokemon.php">Pokemon</a></li>
+        <li><a href="/GroupPHP/challenges.php">Challenges</a></li>
+        <li><a href="/GroupPHP/eliteFour.php">Elite Four</a></li>
+        <li><a href="/GroupPHP/hallOfFame.php">Hall of Fame</a></li>
+        <li><a href="/GroupPHP/items.php"> Items</a></li>
+        <li><a href="/GroupPHP/soldItems.php">Sold Items</a></li>
+        <li><a href="/GroupPHP/itemsUsed.php">Items Used</a></li>
+        <li><a href="/GroupPHP/pokemonSpecies.php">Pokemon Species</a></li>
+        <li><a href="/GroupPHP/pokemonSpeciesTypes.php">Pokemon Species Types</a></li>
+        <li><a href="/GroupPHP/pokemonTypes.php">Pokemon Types</a></li>
+        <li><a href="/GroupPHP/pokemonParticipants.php">Pokemon Participants</a></li>
+    </ul>
 </body>
 </html>
